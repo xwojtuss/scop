b3ae92d69640b1e39f561035ee6467844b47231a
Wojtek Kornatowski <wojkor338@gmail.com>
2026-03-03 11:03:32 +0100

    fixed camera starting at random angle

--- srcs/platform/window/glfw/GLFWWindow.cpp
+++ srcs/platform/window/glfw/GLFWWindow.cpp
@@ -14,6 +14,7 @@ GLFWWindow::GLFWWindow() {
 		glfwSetInputMode(m_window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 
 	setMouseCursorVisible(false);
+	setMouseCursorPositionToCenter();
 	glfwSetInputMode(m_window, GLFW_STICKY_KEYS, GLFW_FALSE);
 
 	glfwSetCursorPosCallback(m_window, cursorPositionCallback);
